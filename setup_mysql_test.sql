d29a2d5018ac6722d359c240d5d827e4adba3b5c
omwami <ianalpha@protonmail.com>
2023-06-22 01:38:22 +0300

    mysql setup

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,7 +1,7 @@
 -- create database hbnb_test_db if not exists
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 
---create the user
+-- create the user
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
 -- grant privileges to user on hbnb_test db

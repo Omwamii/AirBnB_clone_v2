ea92d5d140e2c54bb87a0ab141cd3e9fa323c821
omwami <ianalpha@protonmail.com>
2023-06-22 01:32:37 +0300

    mysql setup

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,12 +1,11 @@
--- script to setup MYSQL server for project
--- create the hbnb_dev_db database
+-- create db hbnb_dev_db if not exists
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
--- create a user on localhost
+-- create the user hbnb_dev
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
--- grant all privileges to the user on hbnb_dev_db
+-- grant user privileges on hbnb_dev_db
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
--- grant select privileges to user on perfomance_schema db
-GRANT SELECT ON perfomance_schema.* TO 'hbnb_dev'@'localhost';
+-- grant user privileges on perfomance_schema db
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,12 +1,11 @@
--- script to setup MYSQL server for project
--- create the hbnb_dev_db database
-CREATE DATABASE IF NOT EXISTS `hbnb_test_db`;
+-- create database hbnb_test_db if not exists
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 
--- create a user on localhost
+--create the user
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
--- grant all privileges to the user on hbnb_dev_db
+-- grant privileges to user on hbnb_test db
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 
--- grant select privileges to user on perfomance_schema db
-GRANT SELECT ON perfomance_schema.* TO 'hbnb_test'@'localhost';
+-- grant user privileges on perfomance_schema db
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';

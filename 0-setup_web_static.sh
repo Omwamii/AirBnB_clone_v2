55a80d2a9e6f482022f51855f9061cedd0a2add8
omwami <ianalpha@protonmail.com>
2023-07-07 11:46:44 +0300

    Setup web static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -13,6 +13,25 @@ sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 sudo chown -R ubuntu:ubuntu /data/
 
-sudo sed -i '/location \/hbnb_static/ { alias \/data\/web_static\/current\/;}' /etc/nginx/sites-available/default
+printf %s "
+server {
+    listen 80;
+    listen [::]:80 default_server;
+    root   /etc/nginx/html;
+    index  index.html;
 
+    location /redirect_me {
+        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+    }
+
+    error_page 404 /error.html;
+    location /error {
+             root /etc/nginx/html;
+             internal;
+     }
+    location /hbnb_static {
+             alias /data/web_static/current/;
+     }
+}
+" > /etc/nginx/sites-enabled/default
 sudo service nginx restart

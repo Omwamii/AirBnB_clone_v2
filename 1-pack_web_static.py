5b5ff060a6491abe4583d4c080faa835a4a6b9db
omwami <ianalpha@protonmail.com>
2023-07-08 17:40:19 +0300

    deploy!

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,21 +3,22 @@
 """
 from fabric.api import local
 from datetime import datetime
+import os
 
 
 def do_pack():
     """ creates a tgz archive of web_static
     """
     # create versions directory if not existing
-    local("mkdir -p versions")
-    now = datetime.now()
-    timestamp = now.strftime("%Y%m%d%H%M%S")
-    tar_file = "web_static_{}.tgz".format(timestamp)
-    target_path = "versions/{}".format(tar_file)
-    # unpack file web_static contents to tar file
-    result = local("tar -czvf {} web_static".format(target_path))
-
-    if result.succeeded:
-        return target_path
-    else:
+    try:
+        now = datetime.now()
+        timestamp = now.strftime("%Y%m%d%H%M%S")
+        local("mkdir -p versions")
+        tar_file = "web_static_{}.tgz".format(timestamp)
+        target_path = "versions/{}".format(tar_file)
+        # unpack file web_static contents to tar file
+        local("tar -czvf {} web_static".format(target_path))
+        print("web_static packed: {} -> \
+                {}Bytes".format(target_path, os.path.getsize(target_path)))
+    except Exception:
         return None
--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,23 +5,24 @@ from fabric.api import local, put, run, env
 from datetime import datetime
 import os
 env.hosts = ['54.84.24.93', '34.224.95.244']
+env.user = 'ubuntu'
 
 
 def do_pack():
     """ creates a tgz archive of web_static
     """
     # create versions directory if not existing
-    local("mkdir -p versions")
-    now = datetime.now()
-    timestamp = now.strftime("%Y%m%d%H%M%S")
-    tar_file = "web_static_{}.tgz".format(timestamp)
-    target_path = "versions/{}".format(tar_file)
-    # unpack file web_static contents to tar file
-    result = local("tar -czvf {} web_static".format(target_path))
-
-    if result.succeeded:
-        return target_path
-    else:
+    try:
+        now = datetime.now()
+        timestamp = now.strftime("%Y%m%d%H%M%S")
+        local("mkdir -p versions")
+        tar_file = "web_static_{}.tgz".format(timestamp)
+        target_path = "versions/{}".format(tar_file)
+        # unpack file web_static contents to tar file
+        local("tar -czvf {} web_static".format(target_path))
+        print("web_static packed: {} -> \
+                {}Bytes".format(target_path, os.path.getsize(target_path)))
+    except Exception:
         return None
 
 
@@ -32,16 +33,17 @@ def do_deploy(archive_path):
         return False
     try:
         _, tar_file = os.path.split(archive_path)
-        _, tar_dir = os.path.splitext(tar_file)
+        tar_dir, _ = os.path.splitext(tar_file)
         put(archive_path, '/tmp/')
         dir_name = '/data/web_static/releases/{}/'.format(tar_dir)
         run('mkdir -p {}'.format(folder_name))
         run('tar -xzf /tmp/{} -C {}'.format(tar_file, dir_name))
         run('rm /tmp/{}'.format(tar_file))
+        run('mv /data/web_static/releases/{}/web_static/* \
+                {}'.format(tar_dir, dir_name))
         run('rm -rf /data/web_static/releases/{}/web_static'.format(tar_dir))
         run('rm -rf /data/web_static/current')
         run('ln -sf {} /data/web_static/current'.format(dir_name))
-
-        return True
-    except:
+        print("New version deployed!")
+    except Exception:
         return False
